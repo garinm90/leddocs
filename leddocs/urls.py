07908402d173c5bb3450ebe468f6c3019d108a87
Max Garin <maxgarin@gmail.com>
2021-01-04 15:09:14 -0500

    Update from other branch

--- leddocs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from users.views import Home
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", Home.as_view())
-    # path("user/", include("users.urls", namespace="user")),
-]

8df3afa2a0949a7761c1ae7705690534ef0096ab
Max Garin <maxgarin@gmail.com>
2021-01-04 16:11:31 -0500

    Add bootstrap 5

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, JobCreateView, JobDetailView, JobListView
 
 
-urlpatterns = [path("", IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("job/<int:id>", JobDetailView.as_view(), name="detail_job"),
+    path("jobs", JobListView.as_view(), name="list_jobs"),
+    path("job", JobCreateView.as_view(), name="create_job"),
+]
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,24 @@
+from django.db import models
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from .models import Job, Customer, Ride, Light
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+
+class JobDetailView(DetailView):
+    model = Job
+    context_object_name = "job"
+
+
+class JobListView(ListView):
+    model = Job
+    context_object_name = "job_list"
+
+
+class JobCreateView(CreateView):
+    model = Job
+    fields = "__all__"

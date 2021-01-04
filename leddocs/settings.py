fcdf1c820377b865d77dc3a6095731a456602400
Max Garin <maxgarin@gmail.com>
2021-01-04 09:13:30 -0500

    Add auth user model and users app

--- leddocs/settings.py
+++ leddocs/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     # My applications
     "jobs",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+AUTH_USER_MODEL = "users.CustomUser"

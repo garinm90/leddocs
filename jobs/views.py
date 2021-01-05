b558d759532b5db0a059ea82b57a1921c4469623
Max Garin <maxgarin@gmail.com>
2021-01-05 09:40:19 -0500

    Add context to look up through table for detail view on Jobs

--- jobs/views.py
+++ jobs/views.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from .models import Job, Customer, Ride, Light
+from .models import Job, Customer, Ride, Light, LightCount
 
 
 class IndexView(TemplateView):
@@ -19,6 +19,14 @@ class IndexView(TemplateView):
 class JobDetailView(DetailView):
     model = Job
     context_object_name = "job"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        job_pk = self.kwargs.get(self.pk_url_kwarg)
+        context["lights"] = LightCount.objects.filter(job__pk=job_pk)
+        return context
 
 
 class JobListView(ListView):

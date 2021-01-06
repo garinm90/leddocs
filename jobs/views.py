aa47043d394ec84dec3dd69ae55372e675de3444
Max Garin <maxgarin@gmail.com>
2021-01-06 08:08:53 -0500

    Chance IndexView to be a redirect view to our Job List

--- jobs/views.py
+++ jobs/views.py
@@ -1,19 +1,12 @@
-from django.db import models
-from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic.base import RedirectView
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Job, Customer, Ride, Light, LightCount
 
 
-class IndexView(TemplateView):
-    template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context["job_list"] = Job.objects.all().order_by("last_updated_date")
-        return context
+class IndexView(RedirectView):
+    url = reverse_lazy("list_jobs")
 
 
 class JobDetailView(DetailView):

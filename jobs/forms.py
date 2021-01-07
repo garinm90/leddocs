aa0d1e00a7c788045dc2d8c0309184efd078347c
Max Garin <maxgarin@gmail.com>
2021-01-06 19:50:19 -0500

    Formatting changes

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from .models import Job, Customer
-from dal.autocomplete import ModelSelect2
+from .models import Job
 
 
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = "__all__"
-        widgets = {"customer": ModelSelect2(url="autocomplete_customer")}
+        widgets = {"customer": forms.CharField()}

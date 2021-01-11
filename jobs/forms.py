3651673d974650631b61ffd7306c46f566ca50be
Max Garin <maxgarin@gmail.com>
2021-01-11 08:42:37 -0500

    Add form for editing jobs

--- jobs/forms.py
+++ jobs/forms.py
@@ -3,8 +3,25 @@ from django import forms
 from dal.autocomplete import ModelSelect2
 from .models import Job
 
+forms.Form
+
 
 class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = "__all__"
+        widgets = {
+            "customer": ModelSelect2(
+                url="autocomplete_customer",
+                attrs={
+                    "data-html": True,
+                },
+            ),
+            "light": forms.widgets.CheckboxSelectMultiple(),
+        }
+
+
+class JobUpdateForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = "__all__"

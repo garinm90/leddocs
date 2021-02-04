19ec31c81fd2edb79d99526a6a22b2f62109ff5c
Max Garin <maxgarin@gmail.com>
2021-02-04 16:19:21 -0500

    Add autocomplete to image upload

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-
+from django.forms import inlineformset_factory
 from dal.autocomplete import ModelSelect2
-from .models import Job
+from .models import Job, Image
 
 forms.Form
 
@@ -34,3 +34,20 @@ class JobUpdateForm(forms.ModelForm):
             ),
             "light": forms.widgets.CheckboxSelectMultiple(),
         }
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = "__all__"
+        widgets = {
+            "job": ModelSelect2(
+                url="autocomplete_job",
+                attrs={
+                    "data-html": True,
+                },
+            )
+        }
+
+
+JobImageFormSet = inlineformset_factory(Job, Image, JobForm)

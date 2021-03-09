9c4b5754cc3cc005ddbcffd711bdccf1891e06a3
Max Garin <maxgarin@gmail.com>
2021-03-09 13:16:32 -0500

    Add crispy forms remove dal
    Modify forms to use djangos default form fields and use jquery select2 to modify them

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,53 +1,67 @@
 from django import forms
 from django.forms import inlineformset_factory
-from dal.autocomplete import ModelSelect2
-from .models import Job, Image
 
-forms.Form
+# from dal.autocomplete import ModelSelect2
+from .models import Customer, Job, Image, Ride
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class CustomerRideForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        exclude = ("company", "primary_contact", "email", "primary_phone_number")
+
+    rides = forms.ChoiceField(
+        widget=forms.SelectMultiple,
+        choices=[(choice.pk, choice) for choice in Ride.objects.all()],
+    )
+
+
+# class JobForm(forms.ModelForm):
+#     class Meta:
+#         model = Job
+#         fields = "__all__"
+#         widgets = {
+#             "customer": ModelSelect2(
+#                 url="autocomplete_customer",
+#                 attrs={
+#                     "data-html": True,
+#                 },
+#             ),
+#             "light": forms.widgets.CheckboxSelectMultiple(),
+#         }
 
 
 class JobForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = "job_form"
+        self.helper.form_method = "post"
+        self.helper.form_action = "create_job"
+        self.helper.add_input(Submit("submit", "Submit"))
+
     class Meta:
         model = Job
         fields = "__all__"
-        widgets = {
-            "customer": ModelSelect2(
-                url="autocomplete_customer",
-                attrs={
-                    "data-html": True,
-                },
-            ),
-            "light": forms.widgets.CheckboxSelectMultiple(),
-        }
+
+    # customer = forms.ChoiceField(
+    #     choices=[(choice.pk, choice) for choice in Customer.objects.all()]
+    # )
 
 
 class JobUpdateForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = "__all__"
-        widgets = {
-            "customer": ModelSelect2(
-                url="autocomplete_customer",
-                attrs={
-                    "data-html": True,
-                },
-            ),
-            "light": forms.widgets.CheckboxSelectMultiple(),
-        }
 
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = "__all__"
-        widgets = {
-            "job": ModelSelect2(
-                url="autocomplete_job",
-                attrs={
-                    "data-html": True,
-                },
-            )
-        }
 
 
 JobImageFormSet = inlineformset_factory(Job, Image, JobForm)
